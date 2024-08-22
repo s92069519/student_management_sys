a632c877260c0eb3da630a8a11b98a49a97438b9
HANEES <s92069519@ousl.lk>
2024-08-22 20:33:59 +0530

    change

--- script/script.js
+++ script/script.js
@@ -33,3 +33,12 @@ searchBtn.addEventListener('click', function (e) {
         }
     }
 });
+
+const toggler = document.getElementById('theme-toggle');
+toggler.addEventListener('change',function (){
+    if(this.checked){
+        document.body.classList.add('dark');
+    }else{
+        document.body.classList.remove('dark');
+    }
+});
